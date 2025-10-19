e1a85f57793a566061cd45b26290e45af3bee852
RaelOliv <kxassa.israel@gmail.com>
2025-10-19 01:22:57 -0300

    All coins av

--- index.js
+++ index.js
@@ -848,7 +848,7 @@ var cryptSymbol = undefined;
 const cryptSymbols = {
     //aion: 'AIONUSDT',
     //algo: 'ALGOUSDT',
-    /*
+    
         aave: 'AAVEUSDT',
         ada: 'ADAUSDT',
         ain: 'AINUSDT',
@@ -899,7 +899,7 @@ const cryptSymbols = {
         wlfi: 'WLFIUSDT',
         xpl: 'XPLUSDT',
         xrp: 'XRPUSDT'
-        */
+        
 /*
     pepe: '1000PEPEUSDT',
     //pump: 'PUMPUSDT',
@@ -918,7 +918,7 @@ const cryptSymbols = {
     wld: 'WLDUSDT',
     ta: 'TAUSDT',
     */
-    vet: 'VETUSDT'
+    //vet: 'VETUSDT'
 
 };
 
