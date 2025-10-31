a988c953b707d91df691058cd4351a15cf5cf176
RaelOliv <kxassa.israel@gmail.com>
2025-10-31 00:40:10 -0300

    Last Sync: 2025-10-31 00:40 (Mobile)

--- workers/margWorker.js
+++ workers/margWorker.js
@@ -1698,7 +1698,7 @@ await salvarCache(balance, 'Balance');
 
   // ---- GATILHOS DE STOP ----
   if (
-    parseFloat(perc) <= parseFloat(-25.0) || parseFloat(percReal) >= parseFloat(1.0)
+    parseFloat(perc) <= parseFloat(-30.0) || parseFloat(percReal) >= parseFloat(1.0)
     /*
     || 
     (oldBalance.maxPercent >= 60.0 && perc <= 50.0) ||
@@ -1790,7 +1790,7 @@ await salvarCache(balance, 'Balance');
 
     } 
     
-    else if (perc < -25.0) {
+    else if (perc < -30.0) {
 
       let res = await fecharTodasPosicoes();
       if (res == true) {
