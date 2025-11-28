c15dd360d76fb90cf112eac65a246acd6a1731ff
RaelOliv <kxassa.israel@gmail.com>
2025-11-28 14:37:33 -0300

    Last Sync: 2025-11-28 14:37 (Mobile)

--- workers/monitorWorker.js
+++ workers/monitorWorker.js
@@ -1912,7 +1912,7 @@ let perc = percentage(
 
 const contPos = await verificarSeTemPosicao(2);
 
-    if (type == 0 && contPos >= 10 ) {
+    if (type == 0 && contPos >= 5 ) {
       parentPort.postMessage(`⚠️ Já existem tres posições abertas. Abortando nova abertura.`);
       return null;
     }
@@ -1940,7 +1940,7 @@ const amtPos = await verificarSeTemPosicao(3);
  if(type == 0 && /*(contPos < 2
       && (parseFloat(perc) >= parseFloat(2.5) || parseFloat(perc) <= parseFloat(-10.0))
       
-      ) || */ contPos < 10){
+      ) || */ contPos < 5){
     const res = await apiAxios.post('/fapi/v1/order', null, {
       params,
       headers: { 'X-MBX-APIKEY': API_KEY },
@@ -3070,7 +3070,7 @@ parentPort.postMessage(`🔎 Perc: ${JSON.stringify(perc)}`);
       (contPos < 2
       && (parseFloat(perc) >= parseFloat(2.5) || parseFloat(perc) <= parseFloat(-10.0))
       
-      ) || */ contPos < 10)
+      ) || */ contPos < 5)
     ) {
 
       //posicaoAberta = 0;
@@ -3201,7 +3201,7 @@ contPos = await verificarSeTemPosicao(2);
         /*
         (contPos < 2
       && (parseFloat(perc) >= parseFloat(2.5) || parseFloat(perc) <= parseFloat(-10.0))
-      ) || */ contPos < 10) {
+      ) || */ contPos < 5) {
           //if (contPos < 1) {
             cacheJson = {
               houveReducao: 0,
