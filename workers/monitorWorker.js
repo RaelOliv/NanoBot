aaa2f6420b9ca3c5abc8c3db7db83e31ce0e0f25
RaelOliv <kxassa.israel@gmail.com>
2025-12-21 17:38:03 -0300

    Update monitorWorker.js

--- workers/monitorWorker.js
+++ workers/monitorWorker.js
@@ -6970,7 +6970,7 @@ async function startWorker() {
   } catch (e) {
     parentPort.postMessage(`❌ Erro fatal no Worker: ${JSON.stringify(e.stack || e.message || e)}
  Reinício em 30s`);
-    setTimeout(startWorker, 30000);
+    setTimeout(startWorker, 60000);
   }
 }
 
