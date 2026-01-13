6f5ee10965435e0fb3ff17c4f859e2694fe24a0b
RaelOliv <kxassa.israel@gmail.com>
2026-01-13 00:53:51 -0300

    Atualizar o monitorWorker.js

--- workers/monitorWorker.js
+++ workers/monitorWorker.js
@@ -5683,12 +5683,12 @@ parseFloat(candles1m.slice(-2)[0].close) >= parseFloat(maiorM3m20p)
 
       ) {
 
-        sideM = 'C';
-        sideOrd = 'BUY';
+        //sideM = 'C';
+        //sideOrd = 'BUY';
 
         //// --- INVERSÃO DO SINAL ---
-        //sideM = 'V';
-        //sideOrd = 'SELL';
+        sideM = 'V';
+        sideOrd = 'SELL';
         //// --- INVERSÃO DO SINAL ---
 
         gatilhoAtivado = true;
@@ -6072,12 +6072,12 @@ parseFloat(candles1m.slice(-2)[0].close) <= parseFloat(menorM3m20p)
 
       ) {
 
-        sideM = 'V';
-        sideOrd = 'SELL';
+        //sideM = 'V';
+        //sideOrd = 'SELL';
 
         //// --- INVERSÃO DO SINAL ---
-        //sideM = 'C';
-        //sideOrd = 'BUY';
+        sideM = 'C';
+        sideOrd = 'BUY';
         //// --- INVERSÃO DO SINAL ---
 
         gatilhoAtivado = true;
