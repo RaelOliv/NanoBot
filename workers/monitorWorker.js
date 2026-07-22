28b9ee0995596976156c5b4d6e792705e1a6b45b
root <root@srv1849756.hstgr.cloud>
2026-07-22 22:29:22 +0000

    Revert "up fix new age"
    This reverts commit f0aedbc732d78a92067a366e28b5581e3d7b2597.

--- workers/monitorWorker.js
+++ workers/monitorWorker.js
@@ -5748,7 +5748,7 @@ parseFloat(candles1m.slice(-2)[0].close) >= parseFloat(maiorM3m20p)
 
         //parseFloat(sRsiLast1h.k) <= parseFloat(50.0) &&
         //parseFloat(sRsiLast1h.k) >= parseFloat(20.0) &&
-        /*
+
         parseFloat(sRsiLast30m.k) <= parseFloat(50.0) &&
         parseFloat(sRsiLast30m.k) >= parseFloat(20.0) &&
 
@@ -5760,20 +5760,12 @@ parseFloat(candles1m.slice(-2)[0].close) >= parseFloat(maiorM3m20p)
 
         parseFloat(sRsiLast30m.k) >= parseFloat(sRsiLast30m_2.k) &&
         parseFloat(sRsiLast30m.d) >= parseFloat(sRsiLast30m_2.d) &&
-*/
-
-        parseFloat(sRsiLast30m.k) <= parseFloat(50.0) &&
-        parseFloat(sRsiLast30m.k) >= parseFloat(20.0) &&
-
-        parseFloat(sRsiLast5m.k) <= parseFloat(20.0) &&
 
         parseFloat(sRsiLast5m.k) >= parseFloat(sRsiLast5m_2.k) &&
         parseFloat(sRsiLast5m.d) >= parseFloat(sRsiLast5m_2.d) &&
 
         parseFloat(sRsiLast3m.k) >= parseFloat(sRsiLast3m_2.k) &&
         parseFloat(sRsiLast3m.d) >= parseFloat(sRsiLast3m_2.d)
-
-        && parseFloat(ema1m100p) >= parseFloat(ema1m100p_2)
 
       ) {
 
@@ -6155,13 +6147,16 @@ parseFloat(candles1m.slice(-2)[0].close) <= parseFloat(menorM3m20p)
           )
         ) &&
 
-        
+        /*
         //parseFloat(sRsiLast1h.k) >= parseFloat(50.0) &&
         //parseFloat(sRsiLast1h.k) <= parseFloat(80.0) &&
+        parseFloat(sRsiLast30m.k) >= parseFloat(50.0) &&
+        parseFloat(sRsiLast30m.k) <= parseFloat(80.0) &&
         //parseFloat(sRsiLast15m.k) >= parseFloat(80.0) &&
+        parseFloat(sRsiLast5m.k) >= parseFloat(80.0) &&
         //parseFloat(sRsiLast3m.k) >= parseFloat(90.0) &&
         //parseFloat(sRsiLast1m.k) >= parseFloat(90.0) 
-        
+        */
 
         //parseFloat(candles1m.slice(-2)[0].high) >= parseFloat(candles1m.slice(-1)[0].high) &&
         /*
@@ -6171,7 +6166,7 @@ parseFloat(candles1m.slice(-2)[0].close) <= parseFloat(menorM3m20p)
         parseFloat(sRsiLast1m.k) < parseFloat(sRsiLast1m_2.k) &&
         parseFloat(sRsiLast1m.k) < parseFloat(sRsiLast1m.d) 
         */
-/*
+
         //parseFloat(preco_atual) < parseFloat(ema1m100p) &&
         //parseFloat(preco_atual) < parseFloat(ema1m5p)
 
@@ -6189,21 +6184,12 @@ parseFloat(candles1m.slice(-2)[0].close) <= parseFloat(menorM3m20p)
 
         parseFloat(sRsiLast30m.k) <= parseFloat(sRsiLast30m_2.k) &&
         parseFloat(sRsiLast30m.d) <= parseFloat(sRsiLast30m_2.d) &&
-*/
-
-        parseFloat(sRsiLast30m.k) >= parseFloat(50.0) &&
-        parseFloat(sRsiLast30m.k) <= parseFloat(80.0) &&
-
-        parseFloat(sRsiLast5m.k) >= parseFloat(80.0) &&
 
         parseFloat(sRsiLast5m.k) <= parseFloat(sRsiLast5m_2.k) &&
         parseFloat(sRsiLast5m.d) <= parseFloat(sRsiLast5m_2.d) &&
         
         parseFloat(sRsiLast3m.k) <= parseFloat(sRsiLast3m_2.k) &&
         parseFloat(sRsiLast3m.d) <= parseFloat(sRsiLast3m_2.d)
-
-        && parseFloat(ema1m100p) <= parseFloat(ema1m100p_2)
-
 
       ) {
 
