540d7f17cf3e36fed649837ec99321c70524af18
root <root@srv1849756.hstgr.cloud>
2026-07-23 00:14:49 +0000

    Reapply "up inv"
    This reverts commit f987d90efc589eb2e25e43dbfd12c7a5ad480a06.

--- workers/monitorWorker.js
+++ workers/monitorWorker.js
@@ -5743,12 +5743,12 @@ parseFloat(candles1m.slice(-2)[0].close) >= parseFloat(maiorM3m20p)
 
       ) {
 
-        sideM = 'C';
-        sideOrd = 'BUY';
+        //sideM = 'C';
+        //sideOrd = 'BUY';
 
         //// --- INVERSÃO DO SINAL ---
-        //sideM = 'V';
-        //sideOrd = 'SELL';
+        sideM = 'V';
+        sideOrd = 'SELL';
         //// --- INVERSÃO DO SINAL ---
 
         gatilhoAtivado = true;
@@ -6134,12 +6134,12 @@ parseFloat(candles1m.slice(-2)[0].close) <= parseFloat(menorM3m20p)
 
       ) {
 
-        sideM = 'V';
-        sideOrd = 'SELL';
+        //sideM = 'V';
+        //sideOrd = 'SELL';
 
         //// --- INVERSÃO DO SINAL ---
-        //sideM = 'C';
-        //sideOrd = 'BUY';
+        sideM = 'C';
+        sideOrd = 'BUY';
         //// --- INVERSÃO DO SINAL ---
 
         gatilhoAtivado = true;
