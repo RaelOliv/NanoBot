bbcf5e3bdcf7b193320b0f0ff05294481f9a28bc
RaelOliv <kxassa.israel@gmail.com>
2026-08-25 10:22:57 -0300

    Last Sync: 2026-08-25 10:22 (Mobile)

--- workers/monitorWorker.js
+++ workers/monitorWorker.js
@@ -5987,8 +5987,9 @@ parseFloat(candles1m.slice(-2)[0].close) >= parseFloat(maiorM3m20p)
 */
 
         
-        //parseFloat(sRsiLast30m.d) >= parseFloat(40.0) &&
-        //parseFloat(sRsiLast30m.d) <= parseFloat(70.0) &&
+        parseFloat(sRsiLast30m.d) >= parseFloat(20.0) &&
+        parseFloat(sRsiLast30m.d) <= parseFloat(50.0) &&
+        
         parseFloat(sRsiLast30m.d) > parseFloat(sRsiLast30m_2.d) &&
         parseFloat(sRsiLast30m.k) > parseFloat(sRsiLast30m_2.k) &&
         parseFloat(sRsiLast30m.k) > parseFloat(sRsiLast30m.d) &&
@@ -6424,11 +6425,11 @@ parseFloat(candles1m.slice(-2)[0].close) <= parseFloat(menorM3m20p)
         parseFloat(sRsiLast30m.d) <= parseFloat(sRsiLast30m_2.d) &&
 */
 
-        //parseFloat(sRsiLast30m.d) <= parseFloat(60.0) &&
-        //parseFloat(sRsiLast30m.d) >= parseFloat(30.0) &&
+        parseFloat(sRsiLast30m.d) <= parseFloat(80.0) &&
+        parseFloat(sRsiLast30m.d) >= parseFloat(50.0) &&
         parseFloat(sRsiLast30m.d) < parseFloat(sRsiLast30m_2.d) &&
 
-        parseFloat(sRsiLast30m.k) < parseFloat(sRsiLast30m.d) &&
+        //parseFloat(sRsiLast30m.k) < parseFloat(sRsiLast30m.d) &&
         (parseFloat(sRsiLast30m.k) - parseFloat(sRsiLast30m.d) <=
         parseFloat(sRsiLast30m_2.k) - parseFloat(sRsiLast30m_2.d)) &&
         
