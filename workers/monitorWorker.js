222a7f83ba9a1ab0cf6266491b6ffc0e981e3b21
RaelOliv <kxassa.israel@gmail.com>
2026-08-28 14:28:35 -0300

    Last Sync: 2026-08-28 14:28 (Mobile)

--- workers/monitorWorker.js
+++ workers/monitorWorker.js
@@ -5999,9 +5999,10 @@ parseFloat(candles1m.slice(-2)[0].close) >= parseFloat(maiorM3m20p)
         //parseFloat(sRsiLast30m.k) <= parseFloat(50.0) &&
         //parseFloat(sRsiLast30m.k) >= parseFloat(20.0) &&
 
-        parseFloat(sRsiLast5m.k) <= parseFloat(20.0) &&
+        parseFloat(sRsiLast5m.d) <= parseFloat(20.0) &&
+        parseFloat(sRsiLast5m.k) >= parseFloat(20.0) &&
 
-        //parseFloat(sRsiLast5m.k) >= parseFloat(sRsiLast5m_2.k) &&
+        parseFloat(sRsiLast5m.k) >= parseFloat(sRsiLast5m_2.k) &&
         //parseFloat(sRsiLast5m.d) >= parseFloat(sRsiLast5m_2.d) &&
 
         parseFloat(sRsiLast3m.k) >= parseFloat(sRsiLast3m_2.k) &&
@@ -6011,12 +6012,12 @@ parseFloat(candles1m.slice(-2)[0].close) >= parseFloat(maiorM3m20p)
 
       ) {
 
-        //sideM = 'C';
-        //sideOrd = 'BUY';
+        sideM = 'C';
+        sideOrd = 'BUY';
 
         //// --- INVERSÃO DO SINAL ---
-        sideM = 'V';
-        sideOrd = 'SELL';
+        //sideM = 'V';
+        //sideOrd = 'SELL';
         //// --- INVERSÃO DO SINAL ---
 
         gatilhoAtivado = true;
@@ -6437,8 +6438,9 @@ parseFloat(candles1m.slice(-2)[0].close) <= parseFloat(menorM3m20p)
         //parseFloat(sRsiLast30m.k) <= parseFloat(80.0) &&
 
         parseFloat(sRsiLast5m.d) >= parseFloat(80.0) &&
+        parseFloat(sRsiLast5m.k) <= parseFloat(80.0) &&
 
-        //parseFloat(sRsiLast5m.k) <= parseFloat(sRsiLast5m_2.k) &&
+        parseFloat(sRsiLast5m.k) <= parseFloat(sRsiLast5m_2.k) &&
         //parseFloat(sRsiLast5m.d) <= parseFloat(sRsiLast5m_2.d) &&
         
         parseFloat(sRsiLast3m.k) <= parseFloat(sRsiLast3m_2.k) &&
@@ -6449,12 +6451,12 @@ parseFloat(candles1m.slice(-2)[0].close) <= parseFloat(menorM3m20p)
 
       ) {
 
-        //sideM = 'V';
-        //sideOrd = 'SELL';
+        sideM = 'V';
+        sideOrd = 'SELL';
 
         //// --- INVERSÃO DO SINAL ---
-        sideM = 'C';
-        sideOrd = 'BUY';
+        //sideM = 'C';
+        //sideOrd = 'BUY';
         //// --- INVERSÃO DO SINAL ---
 
         gatilhoAtivado = true;
