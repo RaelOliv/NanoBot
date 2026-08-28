1e3517545c1d981da328d148642be81870e88ec4
RaelOliv <kxassa.israel@gmail.com>
2026-08-28 07:31:14 -0300

    Last Sync: 2026-08-28 07:31 (Mobile)

--- workers/monitorWorker.js
+++ workers/monitorWorker.js
@@ -6011,12 +6011,12 @@ parseFloat(candles1m.slice(-2)[0].close) >= parseFloat(maiorM3m20p)
 
       ) {
 
-        sideM = 'C';
-        sideOrd = 'BUY';
+        //sideM = 'C';
+        //sideOrd = 'BUY';
 
         //// --- INVERSÃO DO SINAL ---
-        //sideM = 'V';
-        //sideOrd = 'SELL';
+        sideM = 'V';
+        sideOrd = 'SELL';
         //// --- INVERSÃO DO SINAL ---
 
         gatilhoAtivado = true;
@@ -6449,12 +6449,12 @@ parseFloat(candles1m.slice(-2)[0].close) <= parseFloat(menorM3m20p)
 
       ) {
 
-        sideM = 'V';
-        sideOrd = 'SELL';
+        //sideM = 'V';
+        //sideOrd = 'SELL';
 
         //// --- INVERSÃO DO SINAL ---
-        //sideM = 'C';
-        //sideOrd = 'BUY';
+        sideM = 'C';
+        sideOrd = 'BUY';
         //// --- INVERSÃO DO SINAL ---
 
         gatilhoAtivado = true;
