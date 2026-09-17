fd3997751544e607b2149e7748ce5afe113c1a6d
RaelOliv <kxassa.israel@gmail.com>
2026-09-17 16:08:53 -0300

    Last Sync: 2026-09-17 16:08 (Mobile)

--- workers/monitorWorker.js
+++ workers/monitorWorker.js
@@ -6026,8 +6026,9 @@ parseFloat(candles1m.slice(-2)[0].close) >= parseFloat(maiorM3m20p)
       && parseFloat(preco_atual) <= parseFloat(ema1m100p)
 */
     
-        parseFloat(sRsiLast5m.k) >= parseFloat(sRsiLast5m_2.k) &&
-        parseFloat(sRsiLast5m.k) >= parseFloat(sRsiLast5m_2.d) &&
+        //parseFloat(sRsiLast5m.k) >= parseFloat(sRsiLast5m_2.k) &&
+        //parseFloat(sRsiLast5m.k) >= parseFloat(sRsiLast5m_2.d) &&
+        parseFloat(sRsiLast3m.k) <= parseFloat(70.0) &&
         parseFloat(sRsiLast3m.k) >= parseFloat(sRsiLast3m_2.k) &&
         parseFloat(sRsiLast3m.k) >= parseFloat(sRsiLast3m_2.d) &&
 
@@ -6493,8 +6494,9 @@ parseFloat(candles1m.slice(-2)[0].close) <= parseFloat(menorM3m20p)
 */
 
         
-        parseFloat(sRsiLast5m.k) <= parseFloat(sRsiLast5m_2.k) &&
-        parseFloat(sRsiLast5m.k) <= parseFloat(sRsiLast5m_2.d) &&
+        //parseFloat(sRsiLast5m.k) <= parseFloat(sRsiLast5m_2.k) &&
+        //parseFloat(sRsiLast5m.k) <= parseFloat(sRsiLast5m_2.d) &&
+        parseFloat(sRsiLast3m.k) >= parseFloat(30.0) &&
         parseFloat(sRsiLast3m.k) <= parseFloat(sRsiLast3m_2.k) &&
         parseFloat(sRsiLast3m.k) <= parseFloat(sRsiLast3m_2.d) &&
 
