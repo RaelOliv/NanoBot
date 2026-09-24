aa8700683b59e52f69e67187f6bead30af2a7887
RaelOliv <kxassa.israel@gmail.com>
2026-09-24 04:24:36 -0300

    Last Sync: 2026-09-24 04:24 (Mobile)

--- workers/monitorWorker.js
+++ workers/monitorWorker.js
@@ -6039,14 +6039,37 @@ parseFloat(candles1m.slice(-2)[0].close) >= parseFloat(maiorM3m20p)
         parseFloat(preco_atual) <= parseFloat(ema3m5p)
 
        */
+        /*
 
         parseFloat(ema1m250p) > parseFloat(ema1m400p) && 
         parseFloat(ema3m5p_2) <= parseFloat(ema1m400p) && 
         parseFloat(ema3m10p) <= parseFloat(ema1m400p) && 
         parseFloat(ema3m5p) > parseFloat(ema3m5p_2) 
 
+        */
 
-
+        parseFloat(ema1m250p) > parseFloat(ema1m400p) && 
+        parseFloat(candles1m.slice(-2)[0].close) >= (parseFloat(candles1m.slice(-3)[0].open) &&
+        parseFloat(candles1m.slice(-3)[0].close) )
+        && 
+        parseFloat(candles1m.slice(-2)[0].open) <= parseFloat(candles1m.slice(-2)[0].close)
+        &&
+        parseFloat(candles1m.slice(-2)[0].low) >= parseFloat(candles1m.slice(-3)[0].low)
+        &&
+        parseFloat(preco_atual) >
+        parseFloat(candles1m.slice(-2)[0].close)
+        &&
+        (parseFloat(candles1m.slice(-3)[0].low) ||
+        parseFloat(candles1m.slice(-2)[0].low)) >= 
+        parseFloat(ema3m10p)
+        &&
+        (parseFloat(candles1m.slice(-3)[0].low) ||
+        parseFloat(candles1m.slice(-2)[0].low)) <= 
+        parseFloat(ema3m5p)
+        &&
+        parseFloat(ema3m5p) > parseFloat(ema3m5p_2) 
+        
+        
       ) {
 
         sideM = 'C';
@@ -6515,12 +6538,37 @@ parseFloat(candles1m.slice(-2)[0].close) <= parseFloat(menorM3m20p)
         parseFloat(preco_atual) >= parseFloat(ema3m5p)
 */
 
+      /*
         parseFloat(ema1m250p) < parseFloat(ema1m400p) && 
         parseFloat(ema3m5p_2) >= parseFloat(ema1m400p) && 
         parseFloat(ema3m10p) >= parseFloat(ema1m400p) && 
         parseFloat(ema3m5p) < parseFloat(ema3m5p_2) 
 
 
+      */
+      
+
+       parseFloat(ema1m250p) < parseFloat(ema1m400p) && 
+        parseFloat(candles1m.slice(-2)[0].close) <= (parseFloat(candles1m.slice(-3)[0].open) &&
+        parseFloat(candles1m.slice(-3)[0].close) )
+        && 
+        parseFloat(candles1m.slice(-2)[0].open) >= parseFloat(candles1m.slice(-2)[0].close)
+        &&
+        parseFloat(candles1m.slice(-2)[0].high) <= parseFloat(candles1m.slice(-3)[0].high)
+        &&
+        parseFloat(preco_atual) <
+        parseFloat(candles1m.slice(-2)[0].close)
+        &&
+        (parseFloat(candles1m.slice(-3)[0].high) ||
+        parseFloat(candles1m.slice(-2)[0].high)) <= 
+        parseFloat(ema3m10p)
+        &&
+        (parseFloat(candles1m.slice(-3)[0].high) ||
+        parseFloat(candles1m.slice(-2)[0].high)) >= 
+        parseFloat(ema3m5p)
+        &&
+        parseFloat(ema3m5p) < parseFloat(ema3m5p_2) 
+        
         
       ) {
 
